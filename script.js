74e65ef89fb0c3ef4d041bf7cd967d9f57805254
Ruben Corral <rubencorralsola@gmail.com>
2021-08-20 17:15:59 +0200

    commit de prueba

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 window.onload = () => {
   const canvas = document.querySelector("#canvas");
   const ctx = canvas.getContext("2d");
-
+  //commit de prueba
   const car = new Image();
   car.src = "./images/car.png";
 
   let w = 50;
   let h = 70;
-...
+
   car.onload = () => {
     ctx.drawImage(car, canvas.width / 2 - w / 2, canvas.height - h, w, h);
   };
